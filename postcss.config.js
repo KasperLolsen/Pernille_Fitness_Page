24ac37af70a49f1b64d31f9d676067445eddfdd4
KasperLolsen <kasper.l.olsen2002@gmail.com>
2025-03-10 16:30:41 +0100

    Upgrade Tailwind CSS and PostCSS to latest compatible versions

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss7-compat': {},
-    'postcss-import': {},
-    'postcss-flexbugs-fixes': {},
-    'postcss-preset-env': {
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
-    },
+    tailwindcss: {},
     autoprefixer: {}
   }
 } 
